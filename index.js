6193c62721646f54570912e553149ad2f92bdc06
chirag1702 <chirag.jangid1702@gmail.com>
2021-07-21 12:52:46 +0530

    API completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');;
 const app = express();
 
 app.use(express.json());
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const server = http.createServer(app);
 
 const connection = mysql.createConnection({
+    connectionLimit: 100,
     host: '51.210.156.152',
     user: 'mindbiz1_abroad',
     password: 'HKU\\v2&8Ek24\\N<.',
@@ -28,22 +29,21 @@ app.get('/', (req, res) => {
     res.send('jobs API for mmindbiz.net');
 });
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     let password = req.body.password;
 
-    let fnQuery = `SELECT * FROM Users WHERE email = '${email}';`;
+    let hashedPassword = await HashPassword(password);
 
-    console.log(fnQuery);
+    let fnQuery = `select * from Users where email = "${email}";`;
 
     connection.query(fnQuery, (err, result, fields) => {
         if (err) {
             console.log(err);
         } else {
-            console.log(result);
             if (result.length == 0) {
-                connection.query(`INSERT INTO Users (name, email, password) VALUES ('${name}', '${email}', '${password}');`, (err, reslt) => {
+                connection.query(`INSERT INTO Users (name, email, password) VALUES ('${name}', '${email}', '${hashedPassword}');`, (err, reslt) => {
                     if (err) {
                         console.log(err);
                     } else {
@@ -57,6 +57,66 @@ app.post('/signup', (req, res) => {
     });
 });
 
-server.listen(8000, () => {
-    console.log(`Server started on port: ${8000}`);
+app.post('/login', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let fnQuery = `SELECT * FROM Users where email = "${email}";`;
+
+    connection.query(fnQuery, async (err, result, fields) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length == 0) {
+                res.send({ status: 0, message: 'user does not exist by this email!!' });
+            } else {
+                let isCorrectPassword = await bcrypt.compare(password, result[0].password);
+                if (isCorrectPassword) {
+                    res.send({ status: 1, message: 'login successfull', data: result });
+                } else {
+                    res.send({ status: 0, message: 'incorrect password' });
+                }
+            }
+        }
+    });
+});
+
+app.post('/apply_job', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let jobTitle = req.body.jobtitle;
+
+    let fnQuery = `SELECT * FROM job_applications where email = "${email}" AND job_title = "${jobTitle}";`;
+
+    let applyJobQuery = `INSERT INTO job_applications (name, email, job_title) VALUES ('${name}', '${email}', '${jobTitle}');`;
+
+    // connection.query(fnQuery, (err, result) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (result.length == 0) {
+
+    //         } else {
+    //             res.send({ status: 0, message: 'you have already applied for this job!!' });
+    //         }
+    //     }
+    // });
+
+    connection.query(applyJobQuery, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send({ status: 1, message: 'job application submitted successfully!!' });
+        }
+    });
+})
+
+async function HashPassword(password) {
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
+server.listen(2000, () => {
+    console.log(`Server started on port: ${2000}`);
 });
